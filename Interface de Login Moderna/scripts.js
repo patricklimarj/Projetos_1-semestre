9225d7e40deac99a97f867d6a98bdff021cba8c5
patricklimarj <139409368+patricklimarj@users.noreply.github.com>
2023-10-28 11:42:38 -0300

    Delete Interface de Login Moderna directory

--- Interface de Login Moderna/scripts.js
+++
@@ -1,7 +0,0 @@
-const container = document.getElementById("container");
-const registerBtn = document.gerElementById("register");
-const logintBtn = document.getElementById("login");
-
-registerBtn.addEventListener("click", () => {
-  container.classList.add("active");
-});
