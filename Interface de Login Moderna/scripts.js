42bb7bba72c3e5e531e297304d71cc3ad8ae6f89
patricklimarj <139409368+patricklimarj@users.noreply.github.com>
2023-10-28 11:51:08 -0300

    Add files via upload

---
+++ Interface de Login Moderna/scripts.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("container");
+const registerBtn = document.gerElementById("register");
+const logintBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
